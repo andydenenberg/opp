c5a7e1d7b3e84befc416374acc61515b3ad32047
andydenenberg <andydenenberg@gmail.com>
2011-12-13 10:33:20 -0600

    test1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,17 @@ Ospreypoint::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+
+
+
+#  config.assets.compile = false
+
+# ActionView::Template::Error (application.css isn't precompiled): error when set to false
+  config.assets.compile = true    # changed by AMD
+
+
+
+
 
   # Generate digests for assets URLs
   config.assets.digest = true
